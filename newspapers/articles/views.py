f11fcf247a165188b73acc3c09c719e4c5595fca
IsaacVerm <isaacverm@gmail.com>
2019-12-03 14:27:57 +0100

    add articles detail view

--- newspapers/articles/views.py
+++ newspapers/articles/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the articles index.")
+
+
+def detail(request, article_id):
+    return HttpResponse("This is the article with id %s." % article_id)
