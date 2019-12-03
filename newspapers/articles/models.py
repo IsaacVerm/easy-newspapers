95420e139747d203adbe6dbfc48efc2b7338edea
IsaacVerm <isaacverm@gmail.com>
2019-12-03 11:04:11 +0100

    add human-readable name to Article model

--- newspapers/articles/models.py
+++ newspapers/articles/models.py
@@ -5,3 +5,6 @@ class Article(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     description = models.TextField()
+
+    def __str__(self):
+        return self.title
