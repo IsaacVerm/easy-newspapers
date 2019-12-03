07df14fccb827282b362ef6d953c315a30c768c0
IsaacVerm <isaacverm@gmail.com>
2019-12-03 17:09:17 +0100

    remove virtual environment on ci

--- run-web-server.sh
+++ run-web-server.sh
@@ -1,14 +1,5 @@
 #!/bin/bash 
 
-# create virtual environment
-python3 -m venv venv
-
-# activate virtual environment
-source venv/bin/activate
-
-# install packages into it
-pip install
-
 # serve Django web server
 cd newspapers
 python manage.py runserver
