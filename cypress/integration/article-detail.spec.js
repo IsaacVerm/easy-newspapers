9cba18ef2dba1a9808a1f9254ef74b36a3170b27
IsaacVerm <isaacverm@gmail.com>
2019-12-11 16:34:39 +0100

    update article detail view with translations and exhaustive tests

--- cypress/integration/article-detail.spec.js
+++ cypress/integration/article-detail.spec.js
@@ -14,24 +14,24 @@ describe("article detail", () => {
   });
 
   it("open article detail", () => {
-    cy.log("title");
     cy.get("h1")
       .should("contain", titleFirstArticleFeed)
       .should("have.css", "font-size", "54px");
 
-    cy.log("description");
     cy.get("p")
       .should("exist")
       .should("not.contain", "<P>")
       .should("not.contain", "<p>");
+
+    cy.url().should("contain", "dmf");
   });
-  it("go back to articles feed", () => {
-    cy.get('[data-cy="feed_hyperlink"]').click();
-    cy.url().should("contain", "feed");
+  it("go back to index", () => {
+    cy.get('[data-cy="back_to_index"]').click();
+    cy.url().should("match", /articles\/$/);
   });
   it("save article", () => {
     cy.get('[data-cy="save_article_form"]').submit();
     cy.url().should("contain", "saved");
-    cy.get("a").should("contain", "Calvo over De Wever");
+    cy.get("a").should("contain", titleFirstArticleFeed);
   });
 });
