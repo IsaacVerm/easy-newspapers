b73eed0729de8d9d35bfc94eca592f018b377a10
IsaacVerm <isaacverm@gmail.com>
2019-12-11 09:00:30 +0100

    update /article/save url pattern to /save

--- articles/urls.py
+++ articles/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('feed', views.feed, name='feed'),
+    path('save', views.save, name='save'),
     path('saved', views.saved, name='saved'),
-    path('<article_path>', views.detail, name='detail'),
-    path('article/save', views.save, name='save')
+    path('<article_path>', views.detail, name='detail')
 ]
